51e2c9ffe45bc6c3835259ea0098ec1eb7c0a8ed
johntompkins <john.t.tompkins@gmail.com>
2018-03-25 19:13:39 -0400

    Added newsAPI function

---
+++ source/article_collector/newsAPI.py
@@ -0,0 +1,22 @@
+from newsapi import NewsApiClient
+from math import ceil
+
+# fetches urls from NewsAPI of Latest and Most relevant articles regarding supreme court
+def getLatestNewsAPI():
+    articles = []
+    newsapi = NewsApiClient(api_key='43fe19e9160d4a178e862c796a06aea8')
+    newsAPIResult = newsapi.get_everything(q='United States Supreme Court', language='en', page_size=100, sort_by='relevancy')
+    #add articles to list of urls
+    for headline in newsAPIResult['articles']:
+        articles.append(headline['url'])
+    #loop through and request remaining pages
+    for i in range(2, 6):
+        newsAPIResult = newsapi.get_everything(q='United States Supreme Court', language='en', page=i, page_size=100, sort_by='relevancy')
+        for headline in newsAPIResult['articles']:
+            articles.append(headline['url'])
+
+    return articles
+
+def main():
+    getLatestNewsAPI()
+main()
