36f8828a8aeb1beac44acef8c36ded238bda43d0
noneximi <34975451+noneximi@users.noreply.github.com>
2018-03-25 17:26:21 -0400

    edited form tag

--- webapp/index.php
+++ webapp/index.php
@@ -25,13 +25,13 @@
 	</div>
 
 	<div class = "searchbar"><!--the search fields-->
-		<form name = "keySearch" target = "_self" action = ""
+		<form name = "keySearch" action = ""
 		onsubmit = "return validateForm()" method = 'GET'>
 			<input class = "search" type = "text" name = "query"
 			placeholder = "Enter keyword[s]"
 				<?php
 					if(isset($_GET['search_query'])) echo " value='{$_GET['search_query']}'";
-				?> 
+				?>
 			<p id="helpSearch" class="helpB" onmouseover = "onSHelp()"
 			onmouseout = "outSHelp()" onclick = "helpAlert()"><tab1>Help<tab1></p>
 			<br><br>
