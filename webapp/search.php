661fd1e23e495750939ae095de28998890986958
noneximi <34975451+noneximi@users.noreply.github.com>
2018-04-09 03:50:47 -0400

    changed mysqli_connect

--- webapp/search.php
+++ webapp/search.php
@@ -60,9 +60,8 @@
 	<?php
 
 		//connect to database
-		$connect = mysqli_connect("localhost", "root", "cs499") or die(mysqli_connect_error());
+		$connect = mysqli_connect("localhost", "root", "cs499", "cs499SupremeCourt") or die(mysqli_connect_error());
     mysqli_set_charset($connect, "utf8");
-    mysqli_select_db($connect, "cs499SupremeCourt") or die(mysqli_connect_error());
 
 	?>
 
