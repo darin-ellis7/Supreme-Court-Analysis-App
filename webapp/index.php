be4cdce9e20c0dfc66005039f903949baf493d96
noneximi <34975451+noneximi@users.noreply.github.com>
2018-04-09 08:02:44 -0400

    fixed minor syntax error

--- webapp/index.php
+++ webapp/index.php
@@ -66,7 +66,7 @@
                             <!-- php code within these input tags are to remember user input after search is done -->
                             <span class="input-group-btn">
                                 <input class='form-control' type="text" name="search_query" style="width: 430px !important;" placeholder='Type search query here... (leave empty to see all)' <?php if(isset($_GET['search_query'])) echo " value='{$_GET['search_query']}'"; ?> >
-                                <button id="formBut" type='submit' class='btn btn-default' onmouseover='changeSubBut()' onmouseout='reverteSubBut()'
+                                <button id="formBut" type='submit' class='btn btn-default' onmouseover='changeSubBut()' onmouseout='revertSubBut()'
 																style = "height: 30px;
 																font-weight: bold;
 																font-family: monospace;
