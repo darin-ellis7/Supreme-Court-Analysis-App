6e50c917c464b3f37990c0eec4ce22b18ca390d3
ecole96 <ecole96@outlook.com>
2019-09-09 17:09:44 -0400

    updated query for speed and correct top image entity fixes

--- webapp/buildQuery.php
+++ webapp/buildQuery.php
@@ -6,20 +6,28 @@
         $dateTo = mysqli_real_escape_string($connect,$dateTo);
         
         if($mode == 'download') {
-            // old query
-            // $sql = "SELECT DISTINCT title, date, source, author, article.url, article.idArticle, article.score, magnitude, entity, article_text, GROUP_CONCAT(DISTINCT keyword) as keywords, MAX(entity_instances.score) as top_entity FROM (article NATURAL JOIN article_keywords NATURAL JOIN keyword_instances) LEFT JOIN (image NATURAL JOIN image_entities NATURAL JOIN entity_instances) ON article.idArticle = image.idArticle ";
-            $sql = "SELECT article.idArticle,url,article.source,author,title,date,article_text,article.score,magnitude,keywords,top_entity,top_entity_score,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score
+            // old query: $sql = "SELECT DISTINCT title, date, source, author, article.url, article.idArticle, article.score, magnitude, entity, article_text, GROUP_CONCAT(DISTINCT keyword) as keywords, MAX(entity_instances.score) as top_entity FROM (article NATURAL JOIN article_keywords NATURAL JOIN keyword_instances) LEFT JOIN (image NATURAL JOIN image_entities NATURAL JOIN entity_instances) ON article.idArticle = image.idArticle ";
+            $sql = "SELECT article.idArticle,article.url,article.source,author,title,date,article_text,article.score,magnitude,GROUP_CONCAT(DISTINCT keyword) as keywords, entity as top_entity, MAX(entity_instances.score) as top_entity_score, allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score
                     FROM article
-                    NATURAL JOIN (SELECT idArticle,GROUP_CONCAT(keyword) as keywords FROM article_keywords NATURAL JOIN keyword_instances GROUP BY idArticle) k
-                    LEFT JOIN (SELECT idArticle,entity as top_entity, MAX(entity_instances.score) as top_entity_score FROM image NATURAL JOIN image_entities NATURAL JOIN entity_instances GROUP BY idArticle) i ON article.idArticle = i.idArticle
+                    NATURAL JOIN (article_keywords NATURAL JOIN keyword_instances)
+                    LEFT JOIN (image NATURAL JOIN image_entities NATURAL JOIN entity_instances) ON article.idArticle = image.idArticle
                     LEFT JOIN (
-                        (SELECT b1.source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score FROM source_bias b1 INNER JOIN (SELECT source,MIN(allsides_id) min_id FROM source_bias GROUP BY source) b2 ON b2.source=b1.source AND b1.allsides_id = b2.min_id) 
-                        UNION 
-                        (SELECT source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score FROM source_bias WHERE allsides_bias IS NULL and mbfs_bias IS NOT NULL))
-                    bias ON article.source=bias.source ";
+                                (SELECT b1.source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score
+                                FROM source_bias b1
+                                INNER JOIN
+                                    (SELECT source,MIN(allsides_id) min_id
+                                    FROM source_bias
+                                    GROUP BY source) b2 ON b2.source=b1.source
+                                AND b1.allsides_id = b2.min_id)
+                            UNION
+                                (SELECT source,allsides_bias,allsides_confidence,allsides_agree,allsides_disagree,mbfs_bias,mbfs_score
+                                FROM source_bias
+                                WHERE allsides_bias IS NULL
+                                    AND mbfs_bias IS NOT NULL)) bias ON article.source=bias.source ";
         }
         else {
-            $sql = "SELECT DISTINCT date, title, source, idArticle FROM article NATURAL JOIN article_keywords NATURAL JOIN keyword_instances ";
+            // also old: $sql = "SELECT DISTINCT date, title, source, idArticle FROM article NATURAL JOIN article_keywords NATURAL JOIN keyword_instances ";
+            $sql = "SELECT idArticle,source,title,date,GROUP_CONCAT(keyword) as keywords FROM article NATURAL JOIN (article_keywords NATURAL JOIN keyword_instances) ";
             if($mode == 'sourcebox') {
                 $sql = "SELECT source, count(source) FROM (" . $sql;
             }
@@ -27,65 +35,36 @@
 
         $conditionsExist = false; // boolean to determine whether WHERE or AND is used in query statement (if true, initial condition has already been set so subsequent conditions are prefixed with AND)
 
-        // build sql query based on search criteria
-        //if(!empty($search_query) && $mode != "download") // for downloads, search queries require a a HAVING clause at the end rather than a WHERE in the middle, so this block doesn't apply to those
-        if(!empty($search_query))
-        {
-                //$search_query = mysqli_real_escape_string($connect, trim($_GET['search_query']));
-                $query_str = "WHERE (title LIKE '%$search_query%' OR keyword LIKE '%$search_query%') ";
-                $sql .= $query_str;
-                $conditionsExist = true;
-        }
-
         // date range search - if no dates provided, ignore
         if(!empty($dateFrom) && !empty($dateTo)) {
             // convert date input to Y-m-d format - this is because the bootstrap datepicker sends dates in Y/m/d while SQL only accepts as Y-m-d
         	$dateFrom = date("Y-m-d",strtotime($dateFrom));
             $dateTo = date("Y-m-d",strtotime($dateTo));
-
-            if(!$conditionsExist) {
-                $date_str = "WHERE date BETWEEN '$dateFrom' AND '$dateTo' ";
-                $conditionsExist = true;
-            }
-            else {
-                $date_str = "AND date BETWEEN '$dateFrom' AND '$dateTo' ";
-            }
+            $date_str = "WHERE date BETWEEN '$dateFrom' AND '$dateTo' ";
+            $conditionsExist = true;
             $sql .= $date_str;
         }
 
         // if source filter has been applied and search parameters set, limit the sources to what has been checked
         if(!empty($sourcebox) && $mode != 'sourcebox') {
-            if(!$conditionsExist) {
-                $sourceFilter_str = "WHERE article.source in ("; // had to specify article.source here due to ambiguity with source after all the joins in the download query (also seen below)
-                $conditionsExist = true;
-            }
-            else {
-                $sourceFilter_str = "AND article.source in (";
-            }
+            $source_filter_str = !$conditionsExist ? "WHERE " : "AND ";
+            $source_filter_str .= "article.source IN "; 
 
+            $sourcebox_safe = array(); // build an array of sources in SQL string format, ex: ['source1','source2','source3'], all escaped to prevent SQL injections
             foreach($sourcebox as $source) {
-                $sourceFilter_str .= "'" . mysqli_real_escape_string($connect,$source) . "'";
-                if($source != end($sourcebox))
-                {
-                    $sourceFilter_str .= ",";
-                }
+                $source_str = "'" . mysqli_real_escape_string($connect,$source) . "'";
+                array_push($sourcebox_safe,$source_str);
             }
-
-            $sourceFilter_str .= ") ";
-            $sql .= $sourceFilter_str;
+            $source_filter_str .= "(" . implode(",",$sourcebox_safe) . ")"; // implode array by comma delimiter, bookended by parentheses to give us full list of sources to query against in SQL format
+            $sql .= $source_filter_str;
         }
 
-        if($mode == 'sourcebox') {
-        	$sql .= ") AS results GROUP BY SOURCE ORDER BY source";
+        $sql .= "GROUP BY article.idArticle ";
+        if(!empty($search_query)) { // HAVING clause necessary to correctly check search query against list of keywords 
+            $sql .= "HAVING title LIKE '%$search_query%' OR keywords LIKE '%$search_query%' ";
         }
-        else if($mode == 'download') {
-            /*$sql .= "GROUP BY article.idArticle ";
-            if(!empty($search_query)) { // HAVING clause necessary to correctly check search query against list of keywords
-                $sql .= "HAVING title LIKE '%$search_query%' OR keywords LIKE '%$search_query%' ";
-            }*/
-            $sql .= "ORDER BY article.idArticle DESC";
-        }
-
+        if($mode == "download") { $sql .= "ORDER BY article.idArticle DESC"; }
+        else if($mode == 'sourcebox') { $sql .= ") AS results GROUP BY source ORDER BY source"; }
         return $sql;
-	}
+    }
 ?>
